7a55a6c19fc86330875f29e735989834664e18b9
Ashley Grant <ashley@ashleygrant.com>
2015-03-06 15:29:25 -0500

    jaimerivera issue test

--- src/welcome.js
+++ src/welcome.js
@@ -3,6 +3,11 @@ export class Welcome{
     this.heading = 'Welcome to the Aurelia Navigation App!';
     this.firstName = 'John';
     this.lastName = 'Doe';
+    this.borderColor = 'red';
+  }
+
+  get borderColorStyle() {
+    return `border: solid 1px ${this.borderColor};`;
   }
 
   get fullName(){
